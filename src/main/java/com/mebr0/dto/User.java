a78ac687c8de4214bdd8c43a8d04d0c1d9eb5a87
MeBr0 <mebr0mebr1@gmail.com>
2021-07-22 00:41:21 +0600

    fix json schema for User

--- src/main/java/com/mebr0/dto/User.java
+++ src/main/java/com/mebr0/dto/User.java
@@ -11,7 +11,7 @@ import org.apache.camel.Exchange;
 
 import java.util.Map;
 
-@JsonPropertyOrder({ "id", "name", "username", "password", "email", "phone" })
+@JsonPropertyOrder({ "id", "name", "username", "email", "phone" })
 @Getter
 @Setter
 @NoArgsConstructor
@@ -19,23 +19,23 @@ import java.util.Map;
 @Schema(name = "User", description = "User")
 public class User {
 
-    @Schema(name = "id", description = "Unique id", example = "1")
+    @Schema(name = "id", description = "Unique id", required = true, example = "1")
     private Long id;
 
-    @Schema(name = "name", description = "First name", example = "Azamat")
+    @Schema(name = "name", description = "First name", required = true, example = "Azamat")
     private String name;
 
-    @Schema(name = "username", description = "Unique username", example = "mebr0")
+    @Schema(name = "username", description = "Unique username", required = true, example = "mebr0")
     private String username;
 
     @JsonIgnore
-    @Schema(name = "password", description = "Password", example = "qweqweqwe")
+    @Schema(name = "password", description = "Password", hidden = true, example = "qweqweqwe")
     private String password;
 
-    @Schema(name = "email", description = "Unique email", example = "mebr0@gmail.com")
+    @Schema(name = "email", description = "Unique email", required = true, example = "mebr0@gmail.com")
     private String email;
 
-    @Schema(name = "phone", description = "Phone number", example = "7-777-777-7777")
+    @Schema(name = "phone", description = "Phone number", required = true,   example = "7-777-777-7777")
     private String phone;
 
     /**
